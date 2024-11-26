66c8fd085cba89d4401cecab5d03e6b57b068b80
Kallesh-S-Vardi <kalleshvardi@gmail.com>
2024-11-26 01:24:45 -0800

    Create for loop .c

---
+++ for loop .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Using a for loop to print numbers from 1 to 5
+    for (int i = 1; i <= 5; i++) {
+        printf("%d\n", i);  // Print the value of i
+    }
+
+    return 0;
+}
