9704f9f563f61dbbe26163dcbc5823dda8ddf537
Kallesh-S-Vardi <kalleshvardi@gmail.com>
2024-11-25 23:14:05 -0800

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, sum;
+  printf("Enter two numbers");
+ scanf("%d %d", &a, &b);
+  sum = a + b;
+  printf("sum = %d\n",sum);
+  printf("kallesh vardi\n");
+
+  return 0;
+}
