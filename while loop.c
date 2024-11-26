d241534a03a5f38ab48623382639759636f473e5
Kallesh-S-Vardi <kalleshvardi@gmail.com>
2024-11-26 01:23:24 -0800

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    // Using a while loop to print numbers from 1 to 5
+    while (i <= 5) {
+        printf("%d\n", i);  // Print the value of i
+        i++;  // Increment i by 1
+    }
+
+    return 0;
+}
